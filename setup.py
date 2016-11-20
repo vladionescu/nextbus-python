208b0a9de1180f31ba70889235812fb9ba3a5c50
Vlad Ionescu <vlad@vladionescu.com>
2016-11-19 22:09:18 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="NextBus",
+    packages=['nextbus'],
+    version="1.0",
+    description="Fetch NextBus API data",
+    author="Vlad Ionescu",
+    author_email="github@vladionescu.com",
+    url="https://gitlab.com/vladionescu/nextbus-python",
+    install_requires=[
+        "requests>=2.7.0"
+    ]
+)
